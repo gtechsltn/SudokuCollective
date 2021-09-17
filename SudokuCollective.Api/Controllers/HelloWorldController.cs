53265a2a5af183f0a19e050ba45685fab9c5da62
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2021-09-17 15:52:27 -0700

    Remove weather forecast controller and add hello world controller

---
+++ SudokuCollective.Api/Controllers/HelloWorldController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SudokuCollective.Api.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/helloWorld")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new BaseResult
+            {
+                IsSuccess = true,
+                Message = "Status Code 200: Hello World from Sudoku Collective!"
+            };
+
+            return Ok(result);
+        }
+    }
+}
+
+public class BaseResult
+{
+    public bool IsSuccess { get; set; }
+    public bool IsFromCache { get; set; }
+    public string Message { get; set; }
+
+    public BaseResult()
+    {
+        IsSuccess = false;
+        IsFromCache = false;
+        Message = string.Empty;
+    }
+}
