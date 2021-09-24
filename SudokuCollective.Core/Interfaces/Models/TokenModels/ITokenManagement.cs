94f87c16987638f5378c8a786da9c8b92647ac60
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2021-09-24 11:57:22 -0700

    Add token models

---
+++ SudokuCollective.Core/Interfaces/Models/TokenModels/ITokenManagement.cs
@@ -0,0 +1,9 @@
+﻿namespace SudokuCollective.Core.Interfaces.Models.TokenModels
+{
+    public interface ITokenManagement
+    {
+        string Secret { get; set; }
+        string Issuer { get; set; }
+        string Audience { get; set; }
+    }
+}
