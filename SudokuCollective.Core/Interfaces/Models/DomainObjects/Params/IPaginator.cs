b0d71d22a367e8cf7cff81eb6fbf0e0a5621f557
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2021-09-24 11:13:26 -0700

    Add domain object params

---
+++ SudokuCollective.Core/Interfaces/Models/DomainObjects/Params/IPaginator.cs
@@ -0,0 +1,14 @@
+﻿using SudokuCollective.Core.Enums;
+
+namespace SudokuCollective.Core.Interfaces.Models.DomainObjects.Params
+{
+    public interface IPaginator : IDomainObject
+    {
+        int Page { get; set; }
+        int ItemsPerPage { get; set; }
+        SortValue SortBy { get; set; }
+        bool OrderByDescending { get; set; }
+        bool IncludeCompletedGames { get; set; }
+        bool IsNull();
+    }
+}
