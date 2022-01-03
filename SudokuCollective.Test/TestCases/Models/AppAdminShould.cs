24cce58edf13ccd19c7420e30c747fe3c4cd3150
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2022-01-03 10:09:26 -0800

    Add AppAdmin unit tests

---
+++ SudokuCollective.Test/TestCases/Models/AppAdminShould.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using SudokuCollective.Core.Interfaces.Models;
+using SudokuCollective.Core.Interfaces.Models.DomainEntities;
+using SudokuCollective.Core.Models;
+
+namespace SudokuCollective.Test.TestCases.Models
+{
+    public class AppAdminShould
+    {
+        private IAppAdmin? sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new AppAdmin();
+        }
+
+        [Test, Category("Models")]
+        public void ImplementIDomainEntity()
+        {
+            // Arrange and Act
+            if (sut == null)
+            {
+                sut = new AppAdmin();
+            }
+
+            // Assert
+            Assert.That(sut, Is.InstanceOf<IDomainEntity>());
+        }
+
+        [Test, Category("Models")]
+        public void HaveAnID()
+        {
+            // Arrange and Act
+            if (sut == null)
+            {
+                sut = new AppAdmin();
+            }
+
+            // Assert
+            Assert.That(sut.Id, Is.TypeOf<int>());
+            Assert.That(sut.Id, Is.EqualTo(0));
+        }
+
+        [Test, Category("Models")]
+        public void HaveExpectedProperties()
+        {
+            // Arrange and Act
+            if (sut == null)
+            {
+                sut = new AppAdmin();
+            }
+
+            // Assert
+            Assert.That(sut.AppId, Is.TypeOf<int>());
+            Assert.That(sut.UserId, Is.TypeOf<int>());
+            Assert.That(sut.IsActive, Is.TypeOf<bool>());
+        }
+
+        [Test, Category("Models")]
+        public void DefaultToIsActiveTrueStatus()
+        {
+            // Arrange and Act
+            if (sut == null)
+            {
+                sut = new AppAdmin();
+            }
+
+            // Assert
+            Assert.That(sut.IsActive, Is.True);
+        }
+    }
+}
