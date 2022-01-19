8fc386342937c3fe73d7965a85d73144b58b9de4
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2022-01-19 11:02:32 -0800

    Add user role unit tests

--- SudokuCollective.Test/TestCases/Models/UserRoleShould.cs
+++ SudokuCollective.Test/TestCases/Models/UserRoleShould.cs
@@ -5,14 +5,14 @@ using SudokuCollective.Core.Models;
 
 namespace SudokuCollective.Test.TestCases.Models
 {
-    public class UserAppShould
+    public class UserRoleShould
     {
-        private IUserApp? sut;
+        private IUserRole? sut;
 
         [SetUp]
         public void Setup()
         {
-            sut = new UserApp();
+            sut = new UserRole();
         }
 
         [Test, Category("Models")]
@@ -21,7 +21,7 @@ namespace SudokuCollective.Test.TestCases.Models
             // Arrange and Act
             if (sut == null)
             {
-                sut = new UserApp();
+                sut = new UserRole();
             }
 
             // Assert
@@ -34,7 +34,7 @@ namespace SudokuCollective.Test.TestCases.Models
             // Arrange and Act
             if (sut == null)
             {
-                sut = new UserApp();
+                sut = new UserRole();
             }
 
             // Assert
@@ -43,17 +43,17 @@ namespace SudokuCollective.Test.TestCases.Models
         }
 
         [Test, Category("Models")]
-        public void UserIdAndAppIdAreInts()
+        public void UserIdAndRoleIdAreInts()
         {
             // Arrange and Act
             if (sut == null)
             {
-                sut = new UserApp();
+                sut = new UserRole();
             }
 
             // Assert
             Assert.That(sut.UserId, Is.TypeOf<int>());
-            Assert.That(sut.AppId, Is.TypeOf<int>());
+            Assert.That(sut.RoleId, Is.TypeOf<int>());
         }
 
         [Test, Category("Models")]
@@ -62,7 +62,7 @@ namespace SudokuCollective.Test.TestCases.Models
             // Arrange
             if (sut == null)
             {
-                sut = new UserApp();
+                sut = new UserRole();
             }
 
             var user = new User();
@@ -72,7 +72,7 @@ namespace SudokuCollective.Test.TestCases.Models
 
             // Assert
             Assert.That(user, Is.InstanceOf<User>());
-            Assert.That(((UserApp)sut).User, Is.InstanceOf<User>());
+            Assert.That(((UserRole)sut).User, Is.InstanceOf<User>());
         }
 
         [Test, Category("Models")]
@@ -81,17 +81,17 @@ namespace SudokuCollective.Test.TestCases.Models
             // Arrange
             if (sut == null)
             {
-                sut = new UserApp();
+                sut = new UserRole();
             }
 
-            var app = new App();
+            var role = new Role();
 
             // Act
-            sut.App = app;
+            sut.Role = role;
 
             // Assert
-            Assert.That(app, Is.InstanceOf<App>());
-            Assert.That(((UserApp)sut).App, Is.InstanceOf<App>());
+            Assert.That(role, Is.InstanceOf<Role>());
+            Assert.That(((UserRole)sut).Role, Is.InstanceOf<Role>());
         }
 
         [Test, Category("Models")]
@@ -99,15 +99,15 @@ namespace SudokuCollective.Test.TestCases.Models
         {
             // Arrange
             var userId = 2;
-            var appId = 3;
+            var roleId = 3;
 
             // Act
-            sut = new UserApp(userId, appId);
+            sut = new UserRole(userId, roleId);
 
             // Assert
-            Assert.That(sut, Is.InstanceOf<UserApp>());
+            Assert.That(sut, Is.InstanceOf<UserRole>());
             Assert.That(sut.UserId, Is.EqualTo(userId));
-            Assert.That(sut.AppId, Is.EqualTo(appId));
+            Assert.That(sut.RoleId, Is.EqualTo(roleId));
         }
 
         [Test, Category("Models")]
@@ -116,16 +116,16 @@ namespace SudokuCollective.Test.TestCases.Models
             // Arrange
             var id = 1;
             var userId = 2;
-            var appId = 3;
+            var roleId = 3;
 
             // Act
-            sut = new UserApp(id, userId, appId);
+            sut = new UserRole(id, userId, roleId);
 
             // Assert
-            Assert.That(sut, Is.InstanceOf<UserApp>());
+            Assert.That(sut, Is.InstanceOf<UserRole>());
             Assert.That(sut.Id, Is.EqualTo(id));
             Assert.That(sut.UserId, Is.EqualTo(userId));
-            Assert.That(sut.AppId, Is.EqualTo(appId));
+            Assert.That(sut.RoleId, Is.EqualTo(roleId));
         }
     }
 }
