a46d0f78203c04055187bf912d21f036ba56f732
JKing <JKing@moderncampus.com>
2022-01-21 23:29:30 -0800

    Add request unit tests

--- SudokuCollective.Data/Models/Params/Request.cs
+++ SudokuCollective.Data/Models/Params/Request.cs
@@ -1,14 +1,20 @@
-﻿using SudokuCollective.Core.Interfaces.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using SudokuCollective.Core.Interfaces.Models;
 using SudokuCollective.Core.Interfaces.Models.DomainObjects.Params;
 
 namespace SudokuCollective.Data.Models.Params
 {
     public class Request : IRequest
     {
+        [Required]
         public string License { get; set; }
+        [Required]
         public int RequestorId { get; set; }
+        [Required]
         public int AppId { get; set; }
+        [Required]
         public IPaginator Paginator { get; set; }
+        [Required]
         public object DataPacket { get; set; }
 
         public Request()
