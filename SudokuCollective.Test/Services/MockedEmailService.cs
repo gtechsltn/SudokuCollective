c5b15205beb31425e892180ea30cdc3842ebaf71
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2022-01-27 18:07:06 -0800

    Add user service unit tests

---
+++ SudokuCollective.Test/Services/MockedEmailService.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using SudokuCollective.Core.Interfaces.Services;
+
+namespace SudokuCollective.Test.Services
+{
+    public class MockedEmailService
+    {
+        internal Mock<IEmailService> SuccessfulRequest { get; set; }
+        internal Mock<IEmailService> FailedRequest { get; set; }
+
+        public MockedEmailService()
+        {
+            SuccessfulRequest = new Mock<IEmailService>();
+            FailedRequest = new Mock<IEmailService>();
+
+            SuccessfulRequest.Setup(emailService =>
+                emailService.Send(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            FailedRequest.Setup(emailService =>
+                emailService.Send(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(false);
+        }
+    }
+}
