2f05af4d1bfe3d27ddc3467b49c188a18d5618e7
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2022-01-27 21:50:44 -0800

    Add mocked authenticate service

---
+++ SudokuCollective.Test/Services/MockedAuthenticateService.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using SudokuCollective.Core.Interfaces.Models.DomainObjects.Params;
+using SudokuCollective.Core.Interfaces.Services;
+using SudokuCollective.Core.Models;
+using SudokuCollective.Data.Messages;
+using SudokuCollective.Data.Models.Authentication;
+using SudokuCollective.Data.Models.Params;
+using SudokuCollective.Data.Models.Results;
+using SudokuCollective.Test.TestData;
+
+namespace SudokuCollective.Test.Services
+{
+    public class MockedAuthenticateService
+    {
+        internal Mock<IAuthenticateService> SuccessfulRequest { get; set; }
+        internal Mock<IAuthenticateService> FailedRequest { get; set; }
+
+        public MockedAuthenticateService()
+        {
+            SuccessfulRequest = new Mock<IAuthenticateService>();
+            FailedRequest = new Mock<IAuthenticateService>();
+
+            SuccessfulRequest.Setup(authenticateService =>
+                authenticateService.IsAuthenticated(It.IsAny<TokenRequest>()))
+                    .Returns(Task.FromResult(new Result()
+                    {
+                        IsSuccess = true,
+                        Message = UsersMessages.UserFoundMessage,
+                        DataPacket = new List<object>()
+                        {
+                            new AuthenticationResult()
+                            {
+                                Token = TestObjects.GetToken(),
+                                User = new AuthenticatedUser()
+                            }
+                        }
+                    } as IResult));
+
+            FailedRequest.Setup(authenticateService =>
+                authenticateService.IsAuthenticated(It.IsAny<TokenRequest>()))
+                    .Returns(Task.FromResult(new Result()
+                    {
+                        IsSuccess = false,
+                        Message = UsersMessages.UserNotFoundMessage
+                    } as IResult));
+        }
+    }
+}
