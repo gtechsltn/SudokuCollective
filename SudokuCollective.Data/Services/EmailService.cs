5e7174a7a199e448065c7cbb1749366a3baab890
JKing <JKing@moderncampus.com>
2022-03-01 10:41:01 -0800

    Reset email service send false response

--- SudokuCollective.Data/Services/EmailService.cs
+++ SudokuCollective.Data/Services/EmailService.cs
@@ -52,9 +52,9 @@ namespace SudokuCollective.Data.Services
 
                 return true;
             }
-            catch (Exception ex)
+            catch
             {
-                throw ex;
+                return false;
             }
         }
     }
