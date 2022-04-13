706f1a612fd5f5d6a843ea20ef1e69e4e15a68c3
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2022-04-13 16:43:32 -0700

    Move sudoku collective logs project

---
+++ SudokuCollective.Logs/SudokuCollectiveLogger.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace SudokuCollective.Logs
+{
+    public static class SudokuCollectiveLogger
+    {
+        public static void LogInformation<T>(IHttpContextAccessor httpContextAccessor, ILogger<T> logger, EventId eventId, string message)
+        {
+            var data = httpContextAccessor.HttpContext.Request.Form;
+
+            logger.LogInformation(eventId, message);
+        }
+    }
+}
