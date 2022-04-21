80c66da559aebd3ed6794540585abf327b2e79f5
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2022-04-20 23:04:07 -0700

    Add apps controller

---
+++ SudokuCollective.Api/Controllers/V1/AppsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SudokuCollective.Api.Controllers.V1
+{
+    /// <summary>
+    /// App Controller Class
+    /// </summary>
+    [Authorize]
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AppsController : ControllerBase
+    {
+    }
+}
